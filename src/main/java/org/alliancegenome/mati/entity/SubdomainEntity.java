ff6ee1916845143c385a48130ff9d7b503347388
Andres Becerra <abecerra@ebi.ac.uk>
2022-04-26 15:22:11 +0100

    SCRUM-1493: remove unused imports

--- src/main/java/org/alliancegenome/mati/entity/SubdomainEntity.java
+++ src/main/java/org/alliancegenome/mati/entity/SubdomainEntity.java
@@ -1,27 +1,21 @@
 package org.alliancegenome.mati.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import lombok.Data;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.validation.constraints.NotEmpty;
 
 @Entity(name = "Subdomain")
 @Table(name = "subdomain")
 @Data
 public class SubdomainEntity extends PanacheEntity  {
-    @Column @NotEmpty
+    @NotEmpty
     private String code;
 
-    @Column @NotEmpty
+    @NotEmpty
     private String name;
 
-    @Column
     private String description;
 }
