fc67d6b80bdbd5fbd0c3b86a7794cf5731ab08e1
Andres Becerra <abecerra@ebi.ac.uk>
2022-04-27 13:40:06 +0100

    SCRUM-1493: avoid endpoints for subdomains

--- src/main/java/org/alliancegenome/mati/controller/SubdomainResource.java
+++ src/main/java/org/alliancegenome/mati/controller/SubdomainResource.java
@@ -1,7 +1,19 @@
 package org.alliancegenome.mati.controller;
 
 import io.quarkus.hibernate.orm.rest.data.panache.PanacheEntityResource;
+import io.quarkus.rest.data.panache.MethodProperties;
 import org.alliancegenome.mati.entity.SubdomainEntity;
 
-public interface SubdomainResource extends PanacheEntityResource<SubdomainEntity, Long> { }
+import javax.ws.rs.core.Response;
+
+public interface SubdomainResource extends PanacheEntityResource<SubdomainEntity, Long> {
+    @MethodProperties(exposed = false)
+    Response add(Long id);
+
+    @MethodProperties(exposed = false)
+    Response update(Long id);
+
+    @MethodProperties(exposed = false)
+    boolean delete(Long id);
+}
 
