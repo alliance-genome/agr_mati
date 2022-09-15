44502e43ecc8d9a3cd27327f593c33aa9502e63c
Andres Becerra <abecerra@ebi.ac.uk>
2022-09-15 10:53:34 +0100

    SCRUM-2024: add subdomains for lit{resource,reference} and person

---
+++ src/main/resources/db/migration/V0003__SCRUM-2024.sql
@@ -0,0 +1,12 @@
+INSERT INTO subdomain (code, name, description)
+VALUES ('101', 'literature_reference', 'Literature Reference');
+CREATE SEQUENCE subdomain_literature_reference_seq;
+
+INSERT INTO subdomain (code, name, description)
+VALUES ('102', 'literature_resource', 'Literature Resource');
+CREATE SEQUENCE subdomain_literature_resource_seq;
+
+INSERT INTO subdomain (code, name, description)
+VALUES ('103', 'person', 'Person');
+CREATE SEQUENCE subdomain_person_seq;
+
