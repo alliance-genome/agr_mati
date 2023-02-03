5011a749201ba4e24e2740e356b4e3e0b20632f3
Manuel Luypaert <luypaert@ebi.ac.uk>
2023-02-03 14:49:05 +0000

    Re-enabled and improved one-subdomain retrieval endpoint integration test, SCRUM-2517

--- src/test/java/org/alliancegenome/mati/controller/SubdomainResourceITCase.java
+++ src/test/java/org/alliancegenome/mati/controller/SubdomainResourceITCase.java
@@ -1,7 +1,12 @@
 package org.alliancegenome.mati.controller;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
+import io.restassured.response.ValidatableResponse;
+
 import org.alliancegenome.mati.configuration.PostgresResource;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -28,5 +33,17 @@ class SubdomainResourceITCase {
             .get("/api/subdomain")
             .then()
             .statusCode(200);
+    }
+
+    @Test
+    public void getOne() {
+        ValidatableResponse response = given()
+                                       .queryParam("id", "1")
+                                       .when()
+                                       .get("/api/subdomain")
+                                       .then();
+        response.statusCode(200);
+
+        assertThat(response.extract().jsonPath().getList("$").size(), equalTo(1));
     }
 }
