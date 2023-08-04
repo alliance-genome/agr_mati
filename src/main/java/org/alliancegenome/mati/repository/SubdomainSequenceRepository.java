f8f72217ddd2c5821b91746680f998152bff6b9c
Andres Becerra <abecerra@ebi.ac.uk>
2023-08-04 11:38:05 +0100

    SCRUM-3139: add set/get for subdomain counter(s)

--- src/main/java/org/alliancegenome/mati/repository/SubdomainSequenceRepository.java
+++ src/main/java/org/alliancegenome/mati/repository/SubdomainSequenceRepository.java
@@ -1,12 +1,16 @@
 package org.alliancegenome.mati.repository;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.Query;
+import jakarta.persistence.Tuple;
 import lombok.NoArgsConstructor;
 import org.alliancegenome.mati.entity.SubdomainEntity;
 
-import jakarta.inject.Inject;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 /** Manipulates the counters, represented by PostgresSQL sequences, in the database */
@@ -25,7 +29,7 @@ public class SubdomainSequenceRepository {
     public Long getValue(SubdomainEntity subdomainEntity) {
         String sequenceName = "subdomain_" + subdomainEntity.getName() + "_seq";
         String sql = "SELECT COALESCE( (SELECT last_value FROM pg_sequences WHERE sequencename = :sequenceName)," +
-                                      "(SELECT min_value  FROM pg_sequences WHERE sequencename = :sequenceName))";
+                                      "0)";
         Query query = entityManager.createNativeQuery(sql);
         query.setParameter("sequenceName",  sequenceName);
         try {
@@ -65,7 +69,7 @@ public class SubdomainSequenceRepository {
         String sequenceName = "subdomain_" + subdomainEntity.getName() + "_seq";
         String sql = "SELECT SETVAL(:sequenceName, " +
             "(SELECT :value + (SELECT COALESCE( (SELECT last_value FROM pg_sequences WHERE sequencename = :sequenceName)," +
-                                               "(SELECT min_value  FROM pg_sequences WHERE sequencename = :sequenceName)))))";
+                                               "0))))";
         Query query = entityManager.createNativeQuery(sql);
         query.setParameter("sequenceName", sequenceName);
         query.setParameter("value", value);
@@ -74,5 +78,55 @@ public class SubdomainSequenceRepository {
         } catch (Exception e) {
             return -1L;
         }
+    }
+
+    /**
+     * Sets  the subdomain counter to a value
+     * @param subdomain the given subdomain
+     * @param value the value to assign
+     * @return a boolean indicating success
+     */
+    public boolean setSubdomainCounter(SubdomainEntity subdomain, int value) {
+        if (value <= 0 )
+            return false;
+        String sequenceName = "subdomain_" + subdomain.getName() + "_seq";
+        String sql = "SELECT SETVAL(:sequenceName, :value)";
+        Query query = entityManager.createNativeQuery(sql);
+        query.setParameter("sequenceName", sequenceName);
+        query.setParameter("value", value);
+        try {
+            query.getSingleResult();
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Returns the values of all the counters (PostgreSQL sequences)
+     * @return a dictionary {sequence -> value}
+     */
+    public Map<String,Long> getSubdomainCounters() {
+        Map<String,Long> result = new HashMap<>();
+        // This query gets all the counter values
+        String sql = "SELECT sequencename, last_value FROM pg_sequences WHERE sequencename LIKE 'subdomain_%_seq'";
+        Query query = entityManager.createNativeQuery(sql, Tuple.class);
+        try {
+            List<Tuple> tuples = query.getResultList();
+            for (Tuple tuple : tuples) {
+                String pg_sequence = tuple.get("sequencename").toString();
+                Object counter = tuple.get("last_value");
+                // the subdomain table has a key with autoincrement that we ignore here
+                if (!pg_sequence.equals("subdomain_id_seq")) {
+                    int start = "subdomain_".length();
+                    int end = pg_sequence.length() - "_seq".length();
+                    String subdomain = pg_sequence.substring(start, end);
+                    result.put(subdomain, counter==null ? 0 : (Long) counter);
+                }
+            }
+        } catch (Exception e) {
+            return new HashMap<>();
+        }
+        return result;
     }
 }
